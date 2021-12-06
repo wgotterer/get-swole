d6b54f1ce790626b3466433036b63454d60fb8ce
William Gotterer <william.gotterer@gmail.com>
2021-12-06 16:16:27 -0500

    idk

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,5 @@ has_many :online_classes
 has_many :trainers, through: :online_classes
 has_many :private_classes
 has_many :trainers, through: :private_classes
+validates :email,:name,:birth,:goal, presence: true
 end
