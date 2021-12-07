ce1990d50837c2df7691b9a9412af154108996d6
William Gotterer <william.gotterer@gmail.com>
2021-12-07 12:00:05 -0500

    idk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import {Route, Routes} from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Classes from './Classes';
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+import TrainerCard from "./TrainerCard";
+
 
 function Login() {
     const [loginFormData, setLoginFormData] = useState({
@@ -158,6 +160,16 @@ function Login() {
                     type='submit'
                 />
             </form>
+            <div className='Home'>
+            <h2>About Us</h2>
+            <p>Get Swole is the premier online workout app! With Get Swole you can focus on getting a killer workout with trainers that care! Get Swole was founded by three Software Engineers who felt crappy sitting in a chair all day and wanted to <em>get swole</em>!</p>
+            <h2>Our Trainers</h2>
+            {/* Here we will render trainer cards based on data from the database */}
+            <TrainerCard name={'Brendan'} />
+            <TrainerCard name={'Jane'} />
+            <TrainerCard name={'Juliette'} />
+            <TrainerCard name={'William'} />
+         </div>
         </div>
     )
 }
