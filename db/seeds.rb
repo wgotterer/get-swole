5de62444ba6d029f91ce47a90f5e8cfabd4012d4
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-12-10 09:46:01 -0500

    updated the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,27 +77,34 @@ User.create(username:'xavier', password:'xavier', client_id: xavier.id, trainer_
 User.create(username:'yusuf', password:'yusuf', client_id: yusuf.id, trainer_id:'nil')
 User.create(username:'zoe', password:'zoe', client_id: zoe.id, trainer_id:'nil')
 
-juliettepushups = OnlineClass.create(video:"https://youtu.be/4bqJSSLNJ3M", description:"Push up demontration from our most beloved trainer Juliette.", trainer_id:juliette.id, category:"Upper Body", name:"Push ups with Juliette")
+julietteburpees = OnlineClass.create(video:"https://youtu.be/4bqJSSLNJ3M", description:"Burpee demontration from our most beloved trainer Juliette.", trainer_id:juliette.id, category:"Upper Body", name:"Burpees with Juliette")
 juliettefetch = OnlineClass.create(video:"https://youtu.be/eaUoVa08UTI", description:"Watch and learn how to fetch like Juliette.", trainer_id:juliette.id, category:"Weight Loss", name:"Fetching with Juliette")
 julietteyoga = OnlineClass.create(video:'https://youtu.be/iXajQQYkuPM', description:'Learn how to form the downward dog and shavasana poses with Juliette.', trainer_id:juliette.id, category:'Stretching and Flexibility', name:'Yoga with Juliette')
-brendanjumpingjacks = OnlineClass.create(video:'https://youtu.be/R0JtO-Wv3MY', description:'Learn how to do jumping jacks for cardio with Brendan', trainer_id:brendan.id, category:'Weight Loss', name:'Jumping Jacks with Brendan')
+brendanshadowboxing = OnlineClass.create(video:'https://youtu.be/R0JtO-Wv3MY', description:'Learn how to shadow box for cardio with Brendan', trainer_id:brendan.id, category:'Weight Loss', name:'Shadow Boxing with Brendan')
 brendanrunninginplace = OnlineClass.create(video:'https://youtu.be/Zxji0k2Uds0', description:'Watch Brendan practice running in place, the easier way to run (you can watch tv while you do it!!!)', trainer_id:brendan.id, category:'Weight Loss', name:'Running with Brendan')
 brendansquats = OnlineClass.create(video:' https://youtu.be/JPreuNx7hq4', description:'Brendan is back with a squat demonstration starring a special guest', trainer_id:brendan.id, category:'Lower Body', name:'Squats with Brendan')
 williamweightloss = OnlineClass.create(video:'https://www.youtube.com/watch?v=SKGAJDQ3Ks4', description:'50 rounds of 10 jumping jacks, 10 seconds juggling, 10 pushups!', trainer_id:william.id, category:'Weight Loss', name:'Burning Cals with William')
 williamchest = OnlineClass.create(video:'https://youtu.be/7L-6Hckhjy0', description:'1000 band chest presses', trainer_id:william.id, category:'Upper Body', name:'Every Day Is Chest Day')
 williamstretch = OnlineClass.create(video:'https://youtu.be/rOqQIxeLKVQ', description:'Touch your toes, pause for a deep stretch, get a reward, repeat', trainer_id:william.id, category:'Stretching and Flexibility', name:'Stretch with Wilhelm')
 
-
-
-ClientOnlineClass.create( online_class_id: juliettepushups.id, client_id: billy.id)
-ClientOnlineClass.create( online_class_id: juliettepushups.id, client_id: mandy.id)
+ClientOnlineClass.create( online_class_id: julietteburpees.id, client_id: billy.id)
+ClientOnlineClass.create( online_class_id: julietteburpees.id, client_id: mandy.id)
 ClientOnlineClass.create( online_class_id: juliettefetch.id, client_id: grim.id)
 ClientOnlineClass.create( online_class_id: juliettefetch.id, client_id: dad.id)
 
 PrivateClass.create(description:"I want to increase my shoulder strength.",date:"20211210040000",client_id:billy.id,  trainer_id:brendan.id)
+PrivateClass.create(description:'I need some help with my flexibility', date:'20211213090000', client_id:billy.id, trainer_id:jane.id)
+PrivateClass.create(description:'I want to learn to walk around the block', date:'20211219120000', client_id:billy.id, trainer_id:juliette.id)
+PrivateClass.create(description:'I would like to meet the king of flexibility, Wilhelm', date:'20211221120000', client_id:billy.id, trainer_id:william.id)
+PrivateClass.create(description:'I would also like to meet the band man!', date:'20211223', client_id:billy.id, trainer_id:william.id)
+
 PrivateClass.create(description:"I am ready for whatever.",date:"20211215090000",client_id:mandy.id, trainer_id:william.id)
-PrivateClass.create(description:"Need some cuteness.",date:"20211220180000",client_id:dad.id, trainer_id:juliette.id)
 PrivateClass.create(description:"Feeling a litte tight in my hips.",date:"20211210150000",client_id:grim.id, trainer_id:jane.id)
 
-Review.create(content:"Juliette is the best training", rating:"10", online_class_id: juliettepushups.id, client_id: billy.id, client_name: billy.name)
+PrivateClass.create(description:"Need some cuteness.",date:"20211220180000",client_id:dad.id, trainer_id:juliette.id)
+PrivateClass.create(description:'I would like to learn how to do yoga!', date:'20211213120000', client_id:kyle.id, trainer_id:juliette.id)
+PrivateClass.create(description:'Your fetching skills are awesome, teach me', date:'20211215020000', client_id:ivan.id, trainer_id:juliette.id)
+PrivateClass.create(description:'Need some napping pointers', date:'20211225090000', client_id:xavier.id, trainer_id:juliette.id)
+
+Review.create(content:"Juliette is the best training", rating:"10", online_class_id: julietteburpees.id, client_id: billy.id, client_name: billy.name)
 Review.create(content:"I'm walking better now! Thanks Juliette!", rating:"10", online_class_id: juliettefetch.id, client_id: dad.id, client_name: dad.name)
